11017db64b736acd64391211d8762fae3698cc1a
dilsekhpatnaik7 <dpj020@nist.edu>
2024-06-20 00:09:59 +0530

    added

--- client/src/Components/Loader/Loader.tsx
+++ client/src/Components/Loader/Loader.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './Loader.module.css'
-import oligif from '../../assets/Images/loadera.mp4'
+import oligif from '../../assets/Images/Loader-ezgif.com-video-to-gif-converter.gif'
 
 const Loader = () => {
   return (
     <section className={styles.loaderContainer}>
       {/* <video autoPlay loop muted src={olivideo}/> */}
       <div className={styles.loaderBody}>
-        {/* <img src={oligif}/> */}
-        <video autoPlay loop muted src={oligif}/>
+        <img src={oligif}/>
+        {/* <video autoPlay loop muted src={oligif}/> */}
       </div>
       
     </section>
