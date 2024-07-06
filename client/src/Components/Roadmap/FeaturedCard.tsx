12600d35dc1003f7f850d3a9cbbbcaee5ac9a2c0
dilsekhwork <dilsekhed@gmail.com>
2024-07-06 13:30:07 +0530

    added

--- client/src/Components/Roadmap/FeaturedCard.tsx
+++ client/src/Components/Roadmap/FeaturedCard.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { TfeaturedItems } from '../../utils'
 import styles from './Roadmap.module.css'
+import { MdOutlineArrowRightAlt } from 'react-icons/md'
 
 const FeaturedCard = () => {
   return (
     <div className={styles.cardsBody}>
-        <div className={`${styles.card} sticky top-[77vw] lg:top-[25vw] xl:top-[17vw] 2xl:top-[12vw]`} >
+        <div className={`${styles.card} sticky top-[99vw] lg:top-[25vw] xl:top-[17vw] 2xl:top-[12vw]`} >
             <div className={styles.cardBody} style={{top:`calc((${1}*50)px)`}}>
                 <div className={styles.cardHead}><h1>{'Smart Wallet'}</h1></div>
             </div>
             <div className={styles.cardDesc}><p>{'Initially, the Olin Token will be launched on the Base Chain. With the integrated Base Wallet in our application, users can effortlessly create their own self-custodial wallet using their fingertips. Additionally, with the latest Coinbase Smart Wallet enables users to create their wallets easily using passkeys.'}</p></div>
         </div>
 
-        <div className={`${styles.card} sticky top-[87vw] lg:top-[31vw] xl:top-[22vw] 2xl:top-[16vw]`} >
+        <div className={`${styles.card} sticky top-[110vw] lg:top-[31vw] xl:top-[22vw] 2xl:top-[16vw]`} >
             <div className={styles.cardBody} style={{top:`calc((${2}*50)px)`}}>
                 <div className={styles.cardHead}><h1>{`Physical Crypto Gift Cards`}</h1></div>
             </div>
             <div className={styles.cardDesc}><p>{`In a pioneering move, we are introducing physical gift cards for crypto, allowing users to easily load and transfer tokens with unparalleled ease. These gift cards will be available for individuals and institutions alike, facilitating seamless crypto transfers through a familiar physical card model. Imagine the joy of presenting someone with a unique crypto gift, encapsulating the future of finance in a tangible form.`}</p></div>
         </div>
 
-        <div className={`${styles.card} sticky top-[90vw] lg:top-[34vw] xl:top-[26vw] 2xl:top-[30vw]`} >
+        <div className={`${styles.card} sticky top-[130vw] lg:top-[34vw] xl:top-[26vw] 2xl:top-[20vw]`} >
             <div className={styles.cardBody} style={{top:`calc((${3}*50)px)`}}>
                 <div className={styles.cardHead}><h1>{`Bridging the Fiat-Crypto Gap`}</h1></div>
             </div>
--- client/src/Components/Roadmap/Roadmap.tsx
+++ client/src/Components/Roadmap/Roadmap.tsx
@@ -10,7 +10,9 @@ const Roadmap = () => {
         <div className={styles.sectionBody}>
             <div className={styles.headerContent}>
                 <h1 className={styles.headerHeading}>Revolutionizing Crypto Trading <br className='block lg:hidden'/> on Olivia</h1>
-                <p className={styles.headerDesc}>Olivia India is thrilled to introduce Olin, our groundbreaking native cryptocurrency token that will redefine the way you trade and interact with our platform. Olin Token encapsulates our unwavering commitment to innovation, community empowerment, and democratized access to the crypto realm.</p>
+                <div className={styles.headerDescContainer}>
+                    <p className={styles.headerDesc}>Olivia India is thrilled to introduce Olin, our groundbreaking native cryptocurrency token that will redefine the way you trade and interact with our platform. Olin Token encapsulates our unwavering commitment to innovation, community empowerment, and democratized access to the crypto realm.</p>
+                </div>
             </div>
 
             <div className={styles.cardsContainer}>
@@ -20,6 +22,16 @@ const Roadmap = () => {
             </div>
 
             <div className={styles.lastContainer}>
+                    <p className='lg:text-xl text-orange-text'>Join us on this exciting journey as we reshape the crypto trading landscape with Olin Token. Embrace the future of finance, where innovation, community, and accessibility converge to create a truly empowering experience.</p>
+                    <button onClick={() => {
+                        const hero = document.getElementById('navbar');
+                        if (hero) {
+                            hero.scrollIntoView({ behavior: 'smooth' });
+                        }
+                    }} className={styles.joinbutton}>Join Waitlist <MdOutlineArrowRightAlt className={styles.joinIcon}/></button>
+                </div>
+
+            {/* <div className={styles.lastContainer}>
                 <div><p>Join us on this exciting journey as we reshape the crypto trading landscape with Olin Token. Embrace the future of finance, where innovation, community, and accessibility converge to create a truly empowering experience.</p></div>
                 <div className={styles.join}>
                 <button onClick={() => {
@@ -29,7 +41,7 @@ const Roadmap = () => {
                     }
                 }} className={styles.joinbutton}>Join Waitlist <MdOutlineArrowRightAlt className={styles.joinIcon}/></button>
                 </div>
-            </div>
+            </div> */}
         </div>
     </section>
   )
